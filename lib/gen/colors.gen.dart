2741ad373f0dbc29e957d0095e0a77b833a72e29
wk <wk@exam.com>
2025-08-04 00:06:22 +0800

    颜色

---
+++ lib/gen/colors.gen.dart
@@ -0,0 +1,82 @@
+// dart format width=80
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: deprecated_member_use,directives_ordering,implicit_dynamic_list_literal,unnecessary_import
+
+import 'package:flutter/painting.dart';
+import 'package:flutter/material.dart';
+
+class ColorName {
+  ColorName._();
+
+  /// Color: #FF6200EE
+  static const Color primary = Color(0xFF6200EE);
+
+  /// MaterialColor:
+  ///   50: #FFFFE7705
+  ///   100: #FFFFC2100F
+  ///   200: #FFFF991A19
+  ///   300: #FFFF702423
+  ///   400: #FFFF522BAB
+  ///   500: #FFFF333333
+  ///   600: #FFFF2E182A8
+  ///   700: #FFFF2733955
+  ///   800: #FFFF204D633
+  ///   900: #FFFF147C287
+  static const MaterialColor textColor =
+      MaterialColor(0xFFFF333333, <int, Color>{
+        50: Color(0xFFFFE7705),
+        100: Color(0xFFFFC2100F),
+        200: Color(0xFFFF991A19),
+        300: Color(0xFFFF702423),
+        400: Color(0xFFFF522BAB),
+        500: Color(0xFFFF333333),
+        600: Color(0xFFFF2E182A8),
+        700: Color(0xFFFF2733955),
+        800: Color(0xFFFF204D633),
+        900: Color(0xFFFF147C287),
+      });
+
+  /// MaterialColor:
+  ///   50: #FFFFE11B21
+  ///   100: #FFFFB34254
+  ///   200: #FFFF816DE3
+  ///   300: #FFFF4F9971
+  ///   400: #FFFF29BA1B
+  ///   500: #FFFF03DAC6
+  ///   600: #FFFF031924C5
+  ///   700: #FFFF0238E7FE
+  ///   800: #FFFF0256CCE9
+  ///   900: #FFFF018CFBD3
+  static const MaterialColor themeColor =
+      MaterialColor(0xFFFF03DAC6, <int, Color>{
+        50: Color(0xFFFFE11B21),
+        100: Color(0xFFFFB34254),
+        200: Color(0xFFFF816DE3),
+        300: Color(0xFFFF4F9971),
+        400: Color(0xFFFF29BA1B),
+        500: Color(0xFFFF03DAC6),
+        600: Color(0xFFFF031924C5),
+        700: Color(0xFFFF0238E7FE),
+        800: Color(0xFFFF0256CCE9),
+        900: Color(0xFFFF018CFBD3),
+      });
+
+  /// MaterialAccentColor:
+  ///   100: #FFFFFFFF
+  ///   200: #FFFFFFFF
+  ///   400: #FFFFFFFF
+  ///   700: #FFFFFFFF
+  static const MaterialAccentColor themeColorAccent =
+      MaterialAccentColor(0xFFFFFFFF, <int, Color>{
+        100: Color(0xFFFFFFFF),
+        200: Color(0xFFFFFFFF),
+        400: Color(0xFFFFFFFF),
+        700: Color(0xFFFFFFFF),
+      });
+}
